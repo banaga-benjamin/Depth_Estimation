6c8469620d110e50645f95fc3657051a4a4533ea
benjamin-banaga <banaga.benjaminarlantico@gmail.com>
2025-03-30 03:45:32 +0800

    Corrected Sequence Length

--- constants.py
+++ constants.py
@@ -36,7 +36,7 @@ IMG_INTRINSIC_INV = torch.linalg.pinv(IMG_INTRINSIC_MAT)
 
 # constants related to training
 EPOCHS = 1
-SEQ_LEN = 2
+SEQ_LEN = 4
 BATCH_SIZE = 4
 NUM_RANDOM_TRANS = 16 - 1
 NUM_WORKERS = cpu_count( )
